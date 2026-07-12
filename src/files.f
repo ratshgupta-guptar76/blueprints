cf1273202f6a4f7f1e572e8eac5fa784342df94f
Ratish Gupta <guptar76@mcmaster.ca>
2026-07-12 11:00:03 -0400

    Fix files.f module order

--- src/files.f
+++ src/files.f
@@ -1,15 +1,15 @@
 dcim_pkg.sv
-act_shift_chain.sv
-adder_tree.sv
-col_adder.sv
-control_fsm.sv
-dcim_array.sv
-lane_shift_accum.sv
-row_decoder.sv
-shift_accum.sv
 shift_reg.sv
-stream_out.sv
+act_shift_chain.sv
+row_decoder.sv
+dcim_array.sv
+col_adder.sv
+adder_tree.sv
+lane_shift_accum.sv
+shift_accum.sv
 weight_load.sv
-// chip_core.sv
+stream_out.sv
+control_fsm.sv
 dcim_top.sv
-// chip_top.sv
+chip_core.sv
+chip_top.sv
