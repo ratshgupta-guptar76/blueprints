e4832e942165b41e0fa50f6b685829dccfbb33ef
Ratish Gupta <guptar76@mcmaster.ca>
2026-07-18 11:59:56 -0400

    Add row_decoder_tb.py: Only golden reference

--- cocotb/functional/row_decoder_tb.py
+++ cocotb/functional/row_decoder_tb.py
@@ -32,4 +32,40 @@
 # Jul-18-2026 | R. Gupta      | * v1.0   | Initial Testbench Environment Setup
 # ======================================================================================
 
+import cocotb
+
+ROWS = 32 # TODO: read from dut when tests are addded - do not hardcode.
+
+# ---------- Golden Reference ----------
+def golden_ref(addr: int, en: int) -> int:
+    """Computes the golden reference output for the Row Decoder
+
+    Behaviour:
+        enabled: returns a signal with only the bit at the `addr` index set to `1`
+        disabled: returs 0 (all wordlines de-asserted)
+
+    Args:
+        en (`int`) : Enable signal for Row Decoder
+        addr (`int`)    : Binary row address input to decode
+
+    Returns:
+        out (`int`) : The one-hot wordline (wl) decoded bit-vector representation
+    
+    Raises:
+        AssertionError : If `addr` is less than 0 or greater than or equal to `ROWS`
+    """
+
+    if en == 0:
+        return 0
+    assert 0 <= addr < ROWS, f"`addr` {addr} out of range [0,{ROWS})"
+    return 1 << addr
+
+def golden_tb():
+    assert golden_ref(5, 1) == 0b100000
+    assert golden_ref(5, 0) == 0b000000
+    assert golden_ref(0, 1) == 0b000001
+    print("golden_ref self-check passed")
+    return 0
+
+golden_tb()
 
